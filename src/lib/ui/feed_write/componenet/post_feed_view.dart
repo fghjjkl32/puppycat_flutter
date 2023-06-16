4a20b02ab43e2200b67a8f0b3e36dfbaa49db7b4
Geon Woo Jeon <fghjjkl2700@gmail.com>
2023-06-16 18:50:17 +0900

    게시글 본문 입력 필드 관련 기능 구현 완료

--- src/lib/ui/feed_write/componenet/post_feed_view.dart
+++ src/lib/ui/feed_write/componenet/post_feed_view.dart
@@ -1,15 +1,19 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_social_textfield/flutter_social_textfield.dart';
+import 'package:pet_mobile_social_flutter/components/user_list/widget/tag_user_item_widget.dart';
 import 'package:pet_mobile_social_flutter/config/theme/color_data.dart';
 import 'package:pet_mobile_social_flutter/config/theme/text_data.dart';
 import 'package:pet_mobile_social_flutter/ui/feed_write/componenet/cropped_images_list_view.dart';
 import 'package:pet_mobile_social_flutter/ui/feed_write/tag_screen.dart';
 import 'package:pet_mobile_social_flutter/viewmodels/feed_write/feed_write_button_selected_provider.dart';
 import 'package:pet_mobile_social_flutter/viewmodels/feed_write/feed_write_current_tag_count_provider.dart';
+import 'package:pet_mobile_social_flutter/viewmodels/feed_write/feed_write_text_field_height_provider.dart';
 
 class PostFeedView extends ConsumerStatefulWidget {
   const PostFeedView({
@@ -26,9 +30,56 @@ class PostFeedView extends ConsumerStatefulWidget {
 }
 
 class PostFeedViewState extends ConsumerState<PostFeedView> {
+  late final SocialTextEditingController _textEditingController;
+  late final TextRange lastDetectedRange;
+
+  bool isSelectorOpen = false;
+  final FocusNode _focusNode = FocusNode();
+  final ScrollController _scrollController = ScrollController();
+
+  SocialContentDetection lastDetection =
+      SocialContentDetection(DetectedType.plain_text, TextRange.empty, "");
+
+  late final StreamSubscription<SocialContentDetection> _streamSubscription;
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _streamSubscription.cancel();
+    _textEditingController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = SocialTextEditingController()
+      ..setTextStyle(DetectedType.mention,
+          kBody13RegularStyle.copyWith(color: kTextSubTitleColor))
+      ..setTextStyle(DetectedType.hashtag,
+          kBody13RegularStyle.copyWith(color: kTextSubTitleColor));
+
+    _streamSubscription =
+        _textEditingController.subscribeToDetection(onDetectContent);
+  }
+
+  void onDetectContent(SocialContentDetection detection) {
+    lastDetection = detection;
+
+    if (detection.type == DetectedType.mention ||
+        detection.type == DetectedType.hashtag) {
+      ref.watch(feedWriteTextFieldHeightProvider.notifier).state =
+          MediaQuery.of(context).size.height * 0.6;
+    } else {
+      ref.watch(feedWriteTextFieldHeightProvider.notifier).state = 100.h;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final buttonSelected = ref.watch(feedWriteButtonSelectedProvider);
+    var height = MediaQuery.of(context).size.height * 0.5;
 
     return ListView(
       children: <Widget>[
@@ -124,22 +175,39 @@ class PostFeedViewState extends ConsumerState<PostFeedView> {
             ),
           ),
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 12.0.w),
-          child: FormBuilderTextField(
-            maxLength: 200,
-            scrollPadding: EdgeInsets.only(bottom: 400.0.h),
-            maxLines: 6,
-            decoration: InputDecoration(
-                counterText: "",
-                hintText: '내용을 입력해 주세요.\n(게시물 등록 기준에 맞지 않을 경우 게시가 제한될 수 있어요.)',
-                hintStyle:
-                    kBody12RegularStyle.copyWith(color: kNeutralColor500),
-                contentPadding: const EdgeInsets.all(16)),
-            name: 'content',
-            style: kBody13RegularStyle.copyWith(color: kTextSubTitleColor),
-            keyboardType: TextInputType.multiline,
-            textAlignVertical: TextAlignVertical.center,
+        SizedBox(
+          height: ref.watch(feedWriteTextFieldHeightProvider),
+          child: DefaultSocialTextFieldController(
+            focusNode: _focusNode,
+            scrollController: _scrollController,
+            textEditingController: _textEditingController,
+            detectionBuilders: {
+              DetectedType.mention: (context) => mentionContent(height),
+              DetectedType.hashtag: (context) => hashtagContent(height),
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12.0.w),
+              child: FormBuilderTextField(
+                scrollPhysics: const ClampingScrollPhysics(),
+                scrollController: _scrollController,
+                focusNode: _focusNode,
+                controller: _textEditingController,
+                maxLength: 200,
+                scrollPadding: EdgeInsets.only(bottom: 500.h),
+                maxLines: 6,
+                decoration: InputDecoration(
+                    counterText: "",
+                    hintText:
+                        '내용을 입력해 주세요.\n(게시물 등록 기준에 맞지 않을 경우 게시가 제한될 수 있어요.)',
+                    hintStyle:
+                        kBody12RegularStyle.copyWith(color: kNeutralColor500),
+                    contentPadding: const EdgeInsets.all(16)),
+                name: 'content',
+                style: kBody13RegularStyle.copyWith(color: kTextSubTitleColor),
+                keyboardType: TextInputType.multiline,
+                textAlignVertical: TextAlignVertical.center,
+              ),
+            ),
           ),
         ),
         Padding(
@@ -281,6 +349,46 @@ class PostFeedViewState extends ConsumerState<PostFeedView> {
           height: 60.h,
         ),
       ],
+    );
+  }
+
+  PreferredSize mentionContent(double height) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(height),
+      child: ListView.builder(
+        itemBuilder: (context, index) => InkWell(
+          onTap: () {
+            _textEditingController.replaceRange(
+                "@mention_$index", lastDetection.range);
+          },
+          child: TagUserItemWidget(
+            profileImage: 'assets/image/feed/image/sample_image1.png',
+            userName: "@mention_$index",
+            content: '사용자가 설정한 소개글',
+            isSpecialUser: true,
+          ),
+        ),
+      ),
+    );
+  }
+
+  PreferredSize hashtagContent(double height) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(height),
+      child: ListView.builder(
+        itemBuilder: (context, index) => InkWell(
+          onTap: () {
+            _textEditingController.replaceRange(
+                "#hashtag_$index", lastDetection.range);
+          },
+          child: TagUserItemWidget(
+            profileImage: 'assets/image/feed/image/sample_image1.png',
+            userName: "#hashtag_$index",
+            content: '사용자가 설정한 소개글',
+            isSpecialUser: true,
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ src/lib/viewmodels/feed_write/feed_write_text_field_height_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final feedWriteTextFieldHeightProvider = StateProvider<double>((ref) => 200.0);
